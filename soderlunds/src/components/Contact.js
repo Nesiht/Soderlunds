1a20ca2ac36395e3f1ad0da71b314f39b2ee0edd
thisen@gmail.com <thisen@gmail.com>
2021-11-07 14:50:29 +0100

    fixed react reveal effect on contact cards

--- soderlunds/src/components/Contact.js
+++ soderlunds/src/components/Contact.js
@@ -8,16 +8,16 @@ export const Contact = () => {
   return (
     <StyledBackground>
       <StyledHeading>Kontakta oss</StyledHeading>
-      <Fade bottom>
-        <StyledContiner>
+      <StyledContiner>
+        <Fade bottom>
           <ContactCardFront image="../assets/img/045-call.png" alt="Phone" />
           <ContactCardBack title2="070-756 06 45" />
           <ContactCardFront image="../assets/img/043-email-1.png" alt="Letter" />
           <ContactCardBack title2="Dennis@soderlundsplattsattning.se" />
           <ContactCardFront image="../assets/img/040-profile.png" alt="Contact card" />
           <ContactCardBack title="Jaktstigen 13" title2="762 61 Rimbo" title3="Org.nr: 559246-3615" />
-        </StyledContiner>
-      </Fade>
+        </Fade>
+      </StyledContiner>
     </StyledBackground>
   )
 }
