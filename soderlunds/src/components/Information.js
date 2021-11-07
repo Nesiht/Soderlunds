210ae28440c4da49fa89e7f44e58c793a87c204f
thisen@gmail.com <thisen@gmail.com>
2021-11-07 10:48:01 +0100

    added react reveal effect to omoss component

--- soderlunds/src/components/Information.js
+++ soderlunds/src/components/Information.js
@@ -1,11 +1,14 @@
 import React from "react"
+import Fade from 'react-reveal/Fade';
 import { StyledBackground } from "../styles/information"
 import { Omoss } from "./Omoss"
 
 export const Information = () => {
   return (
     <StyledBackground>
-      <Omoss />
+      <Fade bottom>
+        <Omoss />
+      </Fade>
     </StyledBackground>
   )
 }
--- soderlunds/src/components/Omoss.js
+++ soderlunds/src/components/Omoss.js
@@ -1,10 +1,24 @@
 import React from "react"
-import { StyledCard } from "../styles/information"
+import { StyledCard, StyledHeading, StyledParagraph } from "../styles/information"
 
 export const Omoss = () => {
   return (
     <StyledCard>
-      <h2>Om oss</h2>
+      <StyledHeading>Om oss</StyledHeading>
+      <StyledParagraph>
+        Vi er en gruppe av studenter som har valgt å utvikle en nettside som
+        kan hjelpe studenter i Oslo kommune med å finne riktig utstyr.
+        Vi er en gruppe av studenter som har valgt å utvikle en nettside som
+        kan hjelpe studenter i Oslo kommune med å finne riktig utstyr.
+        Vi er en gruppe av studenter som har valgt å utvikle en nettside som
+        kan hjelpe studenter i Oslo kommune med å finne riktig utstyr.
+        Vi er en gruppe av studenter som har valgt å utvikle en nettside som
+        kan hjelpe studenter i Oslo kommune med å finne riktig utstyr.
+        Vi er en gruppe av studenter som har valgt å utvikle en nettside som
+        kan hjelpe studenter i Oslo kommune med å finne riktig utstyr.
+        Vi er en gruppe av studenter som har valgt å utvikle en nettside som
+        kan hjelpe studenter i Oslo kommune med å finne riktig utstyr.
+      </StyledParagraph>
     </StyledCard>
   )
 }
