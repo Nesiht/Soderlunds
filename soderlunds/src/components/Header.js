0fe2c3587e91718f002e8e6f0b268260b29b7a13
thisen@gmail.com <thisen@gmail.com>
2021-11-07 10:11:07 +0100

    created header with logo for mobile

--- soderlunds/src/components/Header.js
+++ soderlunds/src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { HeaderSection, StyledLogo } from '../styles/header'
 
 export const Header = () => {
-  return(
-    <p>Header</p>
+  return (
+    <HeaderSection>
+      <StyledLogo src="../assets/img/Logo_ligg_small_webp.webp" alt="logo" />
+    </HeaderSection>
   )
 }
---
+++ soderlunds/src/styles/header.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const HeaderSection = styled.header`
+  background-color: ;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #e6e6e6;
+  height: 120px;
+  `
+
+export const StyledLogo = styled.img`
+  height: 80px;
+  margin-right: 20px;
+  `
