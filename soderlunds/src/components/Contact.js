4c2a815edda855fd730a5352b3e19cea4792ece2
thisen@gmail.com <thisen@gmail.com>
2021-11-25 21:22:55 +0100

    fixed order on contact cards

--- soderlunds/src/components/Contact.js
+++ soderlunds/src/components/Contact.js
@@ -10,11 +10,11 @@ export const Contact = () => {
       <StyledHeading>Kontakta oss</StyledHeading>
       <Fade bottom>
         <StyledContiner>
-          <ContactCardFront order="-1" image="../assets/img/045-call.png" alt="Phone" />
+          <ContactCardFront order="-3" image="../assets/img/045-call.png" alt="Phone" />
           <ContactCardBack title2="070-756 06 45" />
           <ContactCardFront order="-2" image="../assets/img/043-email-1.png" alt="Letter" />
           <ContactCardBack title2="Dennis@soderlunds.se" />
-          <ContactCardFront order="-3" image="../assets/img/040-profile.png" alt="Contact card" />
+          <ContactCardFront order="-1" image="../assets/img/040-profile.png" alt="Contact card" />
           <ContactCardBack title="Jaktstigen 13" title2="762 61 Rimbo" title3="Org.nr: 559246-3615" />
         </StyledContiner>
       </Fade>
