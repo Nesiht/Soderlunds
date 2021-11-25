fb075383e6902d67e196a130cb04154b1250f871
thisen@gmail.com <thisen@gmail.com>
2021-11-25 18:35:41 +0100

    added style for IG feed

--- soderlunds/src/components/IgFeed.js
+++ soderlunds/src/components/IgFeed.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import InstagramFeed  from 'react-ig-feed'
+import InstagramFeed from 'react-ig-feed'
 import 'react-ig-feed/dist/index.css'
+import { StyledCard } from "../styles/information"
 
 const IG_TOKEN = process.env.REACT_APP_IG_TOKEN
 
 export const IgFeed = () => {
   return (
-      <InstagramFeed token={IG_TOKEN}  counter="99"/>
+    <StyledCard>
+      <InstagramFeed token={IG_TOKEN} counter="12" />
+    </StyledCard>
   )
 }
